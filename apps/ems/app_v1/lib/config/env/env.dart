03d756ad35f671f4e5fc6cef733b7838b40c4b6e
Anderson S. Andrade <asa.sousa@gmail.com>
2026-02-15 10:43:31 -0300

    feat: Configure local proxy with custom SSL and enable debug SSL bypass in Dio, along with new environment variables.

--- apps/ems/app_v1/lib/config/env/env.dart
+++ apps/ems/app_v1/lib/config/env/env.dart
@@ -6,6 +6,8 @@ part 'env.g.dart';
 abstract class Env {
   @EnviedField(varName: 'BACKEND_BASE_URL')
   static const String backendBaseUrl = _Env.backendBaseUrl;
+  @EnviedField(varName: 'BACKEND_REMOTE_URL')
+  static const String backendRemoteUrl = _Env.backendRemoteUrl;
   @EnviedField(varName: 'BACKEND_PATH_API')
   static const String backendPathApi = _Env.backendPathApi;
 }
